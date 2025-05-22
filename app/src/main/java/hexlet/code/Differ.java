c40ea2692432117bee96e7d4e474b30072d553b1
egor_semichev <thebestists@gmail.com>
2025-05-23 00:06:17 +0300

    Перегрузил метод generate для прохождения автотестов

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -9,9 +9,17 @@ import java.util.List;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(Map<String, Object> map1, Map<String, Object> map2) {
-        List<Map<String, Object>> diff = DiffBuilder.build(map1, map2);
-        return new StylishFormatter().format(diff);
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        Map<String, Object> data1 = Parser.getData(filePath1);
+        Map<String, Object> data2 = Parser.getData(filePath2);
+        return generate(data1, data2, "stylish");
+
+    }
+
+    public static String generate(String filePath1, String filePath2, String style) throws Exception {
+        Map<String, Object> data1 = Parser.getData(filePath1);
+        Map<String, Object> data2 = Parser.getData(filePath2);
+        return generate(data1, data2, style);
     }
 
     public static String generate(Map<String, Object> map1, Map<String, Object> map2, String formatName) {
